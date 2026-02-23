140fe1f613a39238fa9f61de9f4027fda46a6fc3
Matsuzaka Yuki <becky.balde@gmail.com>
2026-02-24 01:50:35 +0800

    feat: set fixed window size and disable resizing
    - Change default window size to 480x850 (phone portrait ratio)
    - Remove window resizing capability by removing WS_THICKFRAME and WS_MAXIMIZEBOX styles
    - Use WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX for fixed window

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(900, 700);
+  Win32Window::Size size(480, 850);
   if (!window.Create(L"FolkTool", origin, size)) {
     return EXIT_FAILURE;
   }
